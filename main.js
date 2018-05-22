59c83753f4b3fe131bc0f1c80b86de0fdf28850a
Karen MacPherson <karen@fullstackacademy.com>
2018-05-22 12:51:05 -0400

    update webpack and allow for async-await

--- main.js
+++ main.js
@@ -2,8 +2,10 @@ const db = require('./server/db/_db');
 const app = require('./server');
 const PORT = 1337;
 
-db.sync() // if you update your db schemas, make sure you drop the tables first and then recreate them
-.then(() => {
+const init = async () => {
+  await db.sync() // if you update your db schemas, make sure you drop the tables first and then recreate them
   console.log('db synced')
   app.listen(PORT, () => console.log(`server flyin on port ${PORT}`))
-});
+}
+
+init();
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,25 @@
+'use strict';
+
+const { resolve } = require('path')
+
 module.exports = {
-  entry: './client/main.jsx',
+  entry: ['babel-polyfill', './client/main'],
   output: {
     path: __dirname,
     filename: './public/bundle.js'
   },
+  mode: 'development',
+  context: __dirname,
   devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
   module: {
     rules: [
       {
-        test: /\.jsx?$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader',
-        options: {
-          presets: ['react', 'es2015']
-        }
-      },
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
+        test: /jsx?$/,
+        include: resolve(__dirname, './client'),
+        loader: 'babel-loader'
       }
     ]
   }
